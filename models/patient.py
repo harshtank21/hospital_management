adaeb675f02cf332125ddf6dbbe9f62b56a65c9b
harsh <tankharsh440@gmail.com>
2023-11-28 19:02:53 +0530

    [add]

---
+++ models/patient.py
@@ -0,0 +1,9 @@
+from odoo import models,fields
+
+class Hospital_managemnet(models.Model):
+    _name = "hospital.hospital"
+    _description = "Hospital_managemnet"
+    name=fields.Char(string="Name", required=True)
+    age=fields.Integer(string="age")
+    old_new=fields.Selection([("old","OLD"),("new","new")],string="select")
+    is_balak=fields.Boolean(string="is balak")
